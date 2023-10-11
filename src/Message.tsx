6f44e82be9b5cdddc57fd4af3ad64d12bdb85672
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2023-10-12 00:25:53 +0700

    init

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing
+function Message(){
+    //JSX: JavaScript XML
+    const name = 'ihsan'
+    const nama = "wwww"
+    if(nama)
+        return <h1>Hello World {nama}</h1>
+    return <h1>Hello World</h1>
+}
+
+export default Message;
