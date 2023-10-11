5b4acaf9a41e651ef0dbc497464497356094456f
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2023-10-12 00:30:49 +0700

    modified:   src/main.tsx 	modified:   tsconfig.json

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <React.StrictMode>Cannot find name 'document'. Do you need to change your target library? Try changing the 'lib' compiler option to include 'dom'.ts(2584)
     <App />
   </React.StrictMode>,
 )
